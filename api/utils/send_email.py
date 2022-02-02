554319eb97876c06926dc6023d6d421f328ebf95
gillzon <oscargillberg@gmail.com>
2022-02-02 08:54:36 +0100

    alot

--- api/utils/send_email.py
+++ api/utils/send_email.py
@@ -1,9 +1,9 @@
 import requests
+import os
 
 
 def send_email(stockstatus):
-    key = '4f40d6c46a651e99e3be90e89ee86e43-9ad3eb61-49aa7a99'
-    #key = '7b5599a9cbc97233ca7fef30dbe5d456-0a4b0c40-199144e1'
+    key = os.environ['MAILGUN_KEY']
 
     domain = 'mg.oscargillberg.se'
     recipient = 'oscargillberg@gmail.com'
