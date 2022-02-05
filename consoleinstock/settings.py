51987bd51c9a4e7d78a48d0a2dc2b6550d32980d
gillzon <oscargillberg@gmail.com>
2022-02-05 11:24:57 +0100

    alot

--- consoleinstock/settings.py
+++ consoleinstock/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5d$+wkdq68np6dl)lan*!sh71x6u_lmesp^74vd$ew5yitwhzg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CRONJOBS = [
     ('*/5 * * * *', 'api.cron.runcronjob', '>> /var/log/crontab.log')
